aea59a86e9c774f38b18fa842b98dcca63bef140
Hiroshi Kajisha <kajisha@users.noreply.github.com>
2026-08-29 19:44:05 +0900

    feat: add Quint syntax highlighting queries

---
+++ queries/highlights.scm
@@ -0,0 +1,120 @@
+; Literals and comments
+
+(comment) @comment
+(string) @string
+(integer) @number
+(boolean) @boolean
+(primitive_type) @type.builtin
+
+; Structurally known declarations and call sites
+
+(module
+  name: (_) @module)
+
+(type_declaration
+  name: (qualified_name) @type)
+
+(named_type) @type
+
+(sum_variant
+  name: (_) @constructor)
+
+(variant_pattern
+  name: (_) @constructor)
+
+(const_declaration
+  name: (_) @constant)
+
+(var_declaration
+  name: (_) @variable)
+
+(operator_definition
+  name: (_) @function)
+
+(parameter
+  name: (_) @variable.parameter)
+
+(call_expression
+  function: (_) @function.call)
+
+(ufcs_expression
+  function: (_) @function.call)
+
+; Keywords, scoped to the CST forms in which they are syntax.
+
+(module "module" @keyword)
+(const_declaration "const" @keyword)
+(var_declaration "var" @keyword)
+(assume_declaration "assume" @keyword)
+(type_declaration "type" @keyword)
+
+(operator_qualifier
+  [
+    "val"
+    "def"
+    "pure"
+    "action"
+    "run"
+    "temporal"
+    "nondet"
+  ] @keyword)
+
+(import_declaration
+  ["import" "from" "as"] @keyword)
+
+(export_declaration
+  ["export" "as"] @keyword)
+
+(instance_declaration
+  ["import" "from" "as"] @keyword)
+
+(conditional_expression
+  ["if" "else"] @keyword)
+
+(match_expression "match" @keyword)
+
+(action_block
+  ["all" "any"] @keyword)
+
+; Operators, scoped to expressions, types, and declarations so punctuation
+; with another syntactic role (for example an import wildcard) is not captured.
+
+(unary_expression "-" @operator)
+
+(binary_expression
+  [
+    "^"
+    "*"
+    "/"
+    "%"
+    "+"
+    "-"
+    ">"
+    "<"
+    ">="
+    "<="
+    "!="
+    "=="
+    "and"
+    "or"
+    "iff"
+    "leadsTo"
+    "implies"
+  ] @operator)
+
+(pair_expression "->" @operator)
+(function_type "->" @operator)
+(operator_type "=>" @operator)
+(lambda_expression "=>" @operator)
+(match_arm "=>" @operator)
+
+(delayed_assignment
+  ["'" "="] @operator)
+
+(assume_declaration "=" @operator)
+(operator_definition "=" @operator)
+(type_declaration "=" @operator)
+(instance_override "=" @operator)
+
+(sum_type "|" @operator)
+(match_expression "|" @operator)
